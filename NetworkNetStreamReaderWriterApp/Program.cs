a3bd77e6e80e30653980d7d813a4dbb2b7e18600
maxefimov <efimov.top.academy@gmail.com>
2023-06-17 15:20:19 +0300

    Добавьте файлы проекта.

---
+++ NetworkNetStreamReaderWriterApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+
+using TcpClient client = new();
+string server = "www.yandex.ru";
+await client.ConnectAsync(server, 80);
+
+var stream = client.GetStream();
+var message = $"GET / HTTP/1.1\r\nHost: {server}\r\nConnection: Close\r\n\r\n";
+
+using var writer  = new StreamWriter(stream);
+await writer.WriteAsync(message);
+await writer.FlushAsync();
+
+using var reader = new StreamReader(stream);
+var response = await reader.ReadToEndAsync();
+Console.WriteLine(response);
